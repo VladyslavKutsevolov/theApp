f9716d99434200feaabbcf52274ea7fdd6e211fe
Vladyslav Kutsevolov <kytsevolov@gmail.com>
2021-04-26 13:24:01 -0600

    refactor

--- src/components/collections/Collections.tsx
+++ src/components/collections/Collections.tsx
@@ -1,28 +1,38 @@
 import React, { FC, useEffect, useState } from 'react';
 import CollectionsList from './CollectionsList';
 import CollectionForm from './CollectionForm';
-import { useAppDispatch } from '../../redux/hooks';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import {
   createCollection,
   fetchCollections
 } from '../../redux/collectionsSlice';
 import { CollectionsStateResource } from '../../redux/state/collectionsState';
 import { fetchAllCollections } from '../../api/fakeApi';
+import CollectionDetails from './CollectionDetails';
 
 const Collections: FC = () => {
   const [openForm, setOpenForm] = useState(false);
+  const [collectionList, setCollectionList] = useState<
+    CollectionsStateResource[]
+  >([]);
+  const { collections } = useAppSelector(state => state.collections);
+
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchCollections(fetchAllCollections()));
   }, []);
 
+  useEffect(() => {
+    setCollectionList(collections);
+  }, [collections]);
+
   const createNewCollection = (collection: CollectionsStateResource) => {
     dispatch(createCollection(collection));
   };
 
   return (
-    <div>
+    <>
       <button
         className="m-3 p-2 border-b border-gray-200 sm:rounded-lg bg-purple-500 text-white"
         type="button"
@@ -35,8 +45,8 @@ const Collections: FC = () => {
         createCollection={createNewCollection}
         setOpen={setOpenForm}
       />
-      <CollectionsList />
-    </div>
+      <CollectionsList collections={collectionList} />
+    </>
   );
 };
 
--- src/components/collections/CollectionsList.tsx
+++ src/components/collections/CollectionsList.tsx
@@ -1,77 +1,21 @@
 import React, { FC } from 'react';
 import { useAppSelector } from '../../redux/hooks';
+import { CollectionsStateResource } from '../../redux/state/collectionsState';
+import Collection from './Collection';
 
-const CollectionsList: FC = () => {
-  const { collections } = useAppSelector(state => state.collections);
+interface Props {
+  collections: CollectionsStateResource[];
+}
+
+const CollectionsList = (props: Props) => {
+  const { collections } = props;
+
   return (
-    <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Title
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Description
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Links
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
-                    <span className="sr-only">View Collection</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {collections.map(collection => (
-                  <tr key={collection.id}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">
-                            {collection.name}
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-500">
-                        {collection.description}
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-500">
-                        {collection.links.length}
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <button
-                        type="button"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        View Collection
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      {collections.map(collection => (
+        <Collection key={collection.id} collection={collection} />
+      ))}
+    </>
   );
 };
 
